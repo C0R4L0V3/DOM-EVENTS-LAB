479aa22c230c39263616ae505073819fb0b87f96
C0R4L0V3 <coraline.e.love@gmail.com>
2024-10-23 15:32:42 -0500

    update

---
+++ app.js
@@ -0,0 +1,32 @@
+/*-------------------------------- Constants --------------------------------*/
+ 
+const displayElement = document.querySelector(`.display`)
+const numButtonElement = document.querySelector(`.number`);
+const operateButtonElement = document.querySelector(`.operator`);
+const equalsButtonElement = document.querySelector(`.equals`);
+
+
+
+/*-------------------------------- Variables --------------------------------*/
+
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+numButtonElement.addEventListener('click', () => {
+    console.log(`You Clicked me!`)
+});
+
+operateButtonElement.addEventListener('click', () => {
+
+});
+
+equalsButtonElement.addEventListener('click', () => {
+
+});
+/*-------------------------------- Functions --------------------------------*/
