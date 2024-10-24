1534c631c8aedad5c630b12f54918de618b9f362
C0R4L0V3 <coraline.e.love@gmail.com>
2024-10-24 10:20:50 -0500

    updated with comments

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 /*-------------------------------- Constants --------------------------------*/
 
-const calculator = document.querySelector('#calculator')
-const numButton = document.querySelectorAll(`.number`);
-const operateButton = document.querySelectorAll(`.operator`);
-const equalsButton = document.querySelector(`.equals`);
-const display = document.querySelector(`.display`)
+// const calculator = document.querySelector('#calculator') //<<--- pointless code
+const numButton = document.querySelectorAll(`.number`); //<<-- selects the all buttons with the .number class and puts them in an array
+const operateButton = document.querySelectorAll(`.operator`); //--selectes the all buttons with the .operators class and puts them in an array
+const equalsButton = document.querySelector(`.equals`); //<<--selects the button witht the .equals class
+const display = document.querySelector(`.display`) //<<-- selects the div with the .display class
 
 
 
@@ -18,41 +18,41 @@ const display = document.querySelector(`.display`)
 
 /*-------------------------------- Functions --------------------------------*/
 
-const appendNumOp = () => {
+const appendNumOp = () => {         //<<-- method to put the inner text value of the buttons in the display div
     display.innerText += event.target.innerText
 };
 
-const clearDisplay = () => {
+const clearDisplay = () => {        //<<-- method clears the display div
     display.innerText = ''
 
 };
 
-const calculate = () => {
-    display.innerText = eval(display.innerText); 
+const calculate = () => {       //<<-- methods calculates the total of the string in the display div
+    display.innerText = eval(display.innerText); //<<--- learned about the amazing eval fuction on my own while doing some research refrence: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/eval
 };
-// Evalute function simplifying the backend math for calculator
+
 /*----------------------------- Event Listeners -----------------------------*/
 numButton.forEach((button) => {
-button.addEventListener('click', (event) => {
-    appendNumOp()
+button.addEventListener('click', (event) => { //<<--- event listener to cycle through .number button array
+    appendNumOp()   // method called to display the input
     // console.log(`this is a Num`);
     });
 });
 
-operateButton.forEach((button) => {
+operateButton.forEach((button) => { //<<--event listener to cycle through the .operators array
 button.addEventListener('click', (event) => {
 
-    if (event.target.innerText === 'C'){
-        clearDisplay()
+    if (event.target.innerText === 'C'){  //<<-- if condition to target the 'C' operator button only
+        clearDisplay() //<<-- method to clear display called
     } else {
-        appendNumOp()
+        appendNumOp() //<<-- else method to display operators is called
     }
     // console.log(`This is an operation`)
     });
 });
 
-equalsButton.addEventListener('click', () => {
-    calculate()
+equalsButton.addEventListener('click', () => { //<<-- event listener used on the equals button
+    calculate() //<<-- calculation method called to do the math inside the display div.
     // console.log(`This equals something`)
 });
 
